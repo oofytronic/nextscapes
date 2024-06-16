e36b7c3e497167d917af7489e4ec18f7c10e848e
OOF <oof@motualis.com>
2024-06-16 15:16:41 -0400

    add basic pwa

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,15 @@ export const metadata = {
 const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
+		<head>
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/manifest.json" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#001b2e" />
+			<meta name="msapplication-TileColor" content="#da532c" />
+			<meta name="theme-color" content="#001b2e" />
+		</head>
 		<body className="min-h-dvh flex flex-col">
 			<div className="main">
 				<div className="blur-circle circle1"></div>
