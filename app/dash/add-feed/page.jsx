cce5466d323f43cebd53318357e3f04be11c0de5
OOF <oof@motualis.com>
2024-06-23 14:50:27 -0400

    add category tiles

--- app/dash/add-feed/page.jsx
+++ app/dash/add-feed/page.jsx
@@ -53,6 +53,14 @@ const AddFeed = () => {
         }
     }
 
+    const categories = [
+        {title: 'Technology', image: '/technology.jpg'},
+        {title: 'Business', image: '/business.jpg'},
+        {title: 'Sports', image: '/sports.jpg'},
+        {title: 'Entertainment', image: '/entertainment.jpg'},
+        {title: 'Music', image: '/music.jpg'}
+    ]
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4 w-full h-full">
             <div className="sticky top-0 right-0 z-10 md:col-span-1 order-1 md:order-2 py-4 md:pl-4 border-b md:border-b-none md:border-l">
@@ -80,12 +88,15 @@ const AddFeed = () => {
                     handleSubmit={addFeed}
                 />
                 <p className="text-2xl">Categories</p>
-                <div className="grid md:grid-cols-3 gap-4 mb-4">
-                    {['Technology', 'Business', 'Sports', 'Entertainment', 'Music'].map(category => (
-                        <div key={category} className="flex flex-col gap-4 p-4 border border-gray-500 rounded-md bg-black/50">
-                            <p className="font-bold text-md">{category}</p>
-                        </div>
-                    ))}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                  {categories.map((category, index) => (
+                    <div key={index} className="relative w-full h-64 rounded-md hover:filter hover:sepia transition-all cursor-pointer">
+                      <img src={category.image} alt={category.title} className="object-cover w-full h-full rounded-md" />
+                      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-md">
+                        <p className="text-white font-bold text-lg">{category.title}</p>
+                      </div>
+                    </div>
+                  ))}
                 </div>
             </div>
         </div>
