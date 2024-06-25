1fac09742c80002d255bcb491958fb5d2940b507
oofytronic <oof@motualis.com>
2024-06-25 08:53:47 -0400

    work on feeds list on mobile

--- app/dash/page.jsx
+++ app/dash/page.jsx
@@ -59,47 +59,50 @@ export default function DashboardPage() {
 
     return (
 		<div className="grid grid-cols-1 md:grid-cols-4 md:gap-4 w-full h-full">
-            <div className={`md:col-span-1 order-1 md:order-2 flex flex-col md:items-end gap-4 border-b md:border-b-none md:border-l ${isFeedsExpanded ? 'pb-4' : ''} overflow-y-scroll`}>
-                <div className={`sticky top-0 right-0 bg-black/50 backdrop-blur p-4 ${isFeedsExpanded ? 'border-b' : ''} rounded md:border-b w-full flex justify-end`}>
-                    <button className="flex items-center gap-1 text-white focus:outline-none md:pointer-events-none" onClick={toggleFeeds}>
-                        <p className="font-satoshi font-bold text-lg">Scapes</p>
-                        {isFeedsExpanded ? <FaChevronUp className="h-4 w-4 text-white md:hidden" /> : <FaChevronDown className="h-4 w-4 text-white md:hidden" />}
-                    </button>
-                </div>
-                <div className={`${!isFeedsExpanded ? 'hidden' : 'block'} md:flex md:max-h-full flex gap-2 font-satoshi font-semibold w-full md:flex-wrap md:justify-end`}>
-                    <Feed />
-                </div>
+          {/* Sidebar */}
+          <div className={`md:col-span-1 md:row-start-auto md:row-end-auto row-start-1 row-end-2 order-1 md:order-2 flex flex-col md:items-end gap-4 border-b md:border-b-none md:border-l ${isFeedsExpanded && 'pb-4'} overflow-y-scroll`}>
+            <div className={`sticky top-0 right-0 bg-black/50 backdrop-blur p-4 ${isFeedsExpanded && 'border-b'} rounded md:border-b w-full flex justify-end`}>
+              <button className="flex items-center gap-1 text-white focus:outline-none md:pointer-events-none" onClick={toggleFeeds}>
+                <p className="font-satoshi font-bold text-lg">Scapes</p>
+                {isFeedsExpanded ? <FaChevronUp className="h-4 w-4 text-white md:hidden" /> : <FaChevronDown className="h-4 w-4 text-white md:hidden" />}
+              </button>
             </div>
-            {articles.length === 0 ? (
-                <div className="flex flex-col gap-4 md:col-span-3 order-2 md:order-1 py-4 pl-4">
-                    <p className="font-satoshi font-semibold text-sm">No articles found. Please add feeds.</p>
-                    <Link href="/dash/add-feed" className="light_btn w-fit">
-                        Add Scape
-                    </Link>
-                </div>
-            ) : (
-                <div className="md:col-span-3 order-2 md:order-1 flex flex-col gap-4 py-4 px-4 md:pr-0 h-full overflow-y-auto">
-                    <h1 className="font-satoshi font-bold head_text">Main Collection</h1>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
-                        {articles.map((article, index) => (
-                            <a key={index} href={article.link} target="_blank" rel="noopener noreferrer" className="h-full w-full">
-                                <div className="flex flex-col gap-4 bg-black/50 hover:bg-black transition border border-gray-500 rounded-md p-6 cursor-pointer h-full">
-                                    {article.image && (
-                                        <img src={article.image} alt={article.title} className="w-full h-auto object-contain rounded-md" />
-                                    )}
-                                    <div className="flex flex-col gap-4">
-                                        <p className="text-sm">{article.channel}</p>
-                                        <h2 className="font-inter font-semibold">{article.title}</h2>
-                                        <p className="font-satoshi font-semibold text-sm break-words truncate">
-                                            {article.des}
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        ))}
+            <div className={`${!isFeedsExpanded && 'hidden'} md:flex md:max-h-full flex gap-2 font-satoshi font-semibold w-full md:flex-wrap md:justify-end`}>
+              <Feed />
+            </div>
+          </div>
+
+          {/* Articles */}
+          {articles.length === 0 ? (
+            <div className="flex flex-col gap-4 md:col-span-3 md:row-start-auto md:row-end-auto row-start-2 row-end-3 order-2 md:order-1 py-4 pl-4">
+              <p className="font-satoshi font-semibold text-sm">No articles found. Please add feeds.</p>
+              <Link href="/dash/add-feed" className="light_btn w-fit">
+                Add Scape
+              </Link>
+            </div>
+          ) : (
+            <div className="md:col-span-3 md:row-start-auto md:row-end-auto row-start-2 row-end-3 order-2 md:order-1 flex flex-col gap-4 py-4 px-4 md:pr-0 h-full overflow-y-auto">
+              <h1 className="font-satoshi font-bold head_text">Main Collection</h1>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
+                {articles.map((article, index) => (
+                  <a key={index} href={article.link} target="_blank" rel="noopener noreferrer" className="h-full w-full">
+                    <div className="flex flex-col gap-4 bg-black/50 hover:bg-black transition border border-gray-500 rounded-md p-6 cursor-pointer h-full">
+                      {article.image && (
+                        <img src={article.image} alt={article.title} className="w-full h-auto object-contain rounded-md" />
+                      )}
+                      <div className="flex flex-col gap-4">
+                        <p className="text-sm">{article.channel}</p>
+                        <h2 className="font-inter font-semibold">{article.title}</h2>
+                        <p className="font-satoshi font-semibold text-sm break-words truncate">
+                          {article.des}
+                        </p>
+                      </div>
                     </div>
-                </div>
-            )}
+                  </a>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
 	);
 }
